0777a5cded036adc955357876a110c08a51dec47
huisonglin <huisonglin@DESKTOP-28IOLGT>
2018-12-27 18:37:32 +0800

    xx

--- yjk-app/src/main/java/com/yjk/app/service/wx/template/service/impl/WxComplainTemplateNotifyHandler.java
+++ yjk-app/src/main/java/com/yjk/app/service/wx/template/service/impl/WxComplainTemplateNotifyHandler.java
@@ -1,5 +1,14 @@
 package com.yjk.app.service.wx.template.service.impl;
 
-public class WxComplainTemplateNotifyHandler {
+import com.yjk.app.service.wx.template.request.NotifyRequest;
+import com.yjk.app.service.wx.template.service.WxTemplateNotify;
+
+public class WxComplainTemplateNotifyHandler implements WxTemplateNotify{
+
+	@Override
+	public void doSendTemplateMessage(NotifyRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
