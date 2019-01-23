91b86d6029300afd223e5cd279446e878f8731b0
'huisonglin' <'1141268806@qq.com'>
2019-01-23 09:26:07 +0800

    提交小程序代码

---
+++ yjk-app/src/main/webapp/zkzjx-xcx/app.js
@@ -0,0 +1,57 @@
+//app.js
+const url_microService = require('/config/config').url_microService; //（与config.js相对路径）
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    // 登录
+    var that = this;
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: url_microService +"app/member/loginByWxXcx",
+          data: {
+            code:res.code
+          },
+          header: {},
+          method: 'GET',
+          dataType: 'json',
+          responseType: 'text',
+          success: function(res) {
+            console.log(res)
+            wx.setStorageSync("simpleInfo", res.data.info)
+          },
+          fail: function(res) {},
+          complete: function(res) {},
+        })
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+    simpleInfo: null
+  }
+})
---
+++ yjk-app/src/main/webapp/zkzjx-xcx/pages/index/index.js
@@ -0,0 +1,125 @@
+//index.js
+//获取应用实例
+import Toast from '../../dist/toast/toast';
+const url_microService = require('../../config/config').url_microService; //（与config.js相对路径）
+const app = getApp()
+
+Page({
+  data: {
+    arrayItems:[],
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    picPosition: 0,
+    picLenth:5,
+    currentPage: 1,
+    pageSize : 5
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    var that = this;
+    var currentPage = 0; // 因为数组下标是从0开始的，所以这里用了0
+    that.data.currentPage = 1;
+      wx.request({
+        url: url_microService+'/app/search',
+        data: {
+          pageNo: '1',
+          longitude: '118.8',
+          latitude:'33.7',
+          distance:'1000',
+          type: '1'
+        },
+        header: {},
+        method: 'GET',
+        dataType: 'json',
+        responseType: 'text',
+        success: function(res) {
+          console.log(res)
+          that.setData({
+            ["arrayItems[" + currentPage + "]"]: res.data.info.rows
+          })
+        },
+        fail: function(res) {},
+        complete: function(res) {},
+      })
+  },
+
+  onReady(){
+
+  },
+  switchPic: function(e){
+    var that = this;
+    if(e.detail.source == 'touch'){
+      //防止swiper控件卡死
+      if (e.detail.current == 0 && this.data.picPosition > 1) {//卡死时，重置current为正确索引
+        this.setData({ picPosition: this.data.picPosition });
+      } else {//正常轮转时，记录正确页码索引
+        this.setData({ picPosition: e.detail.current });
+      }
+      // console.log(this.data.picPosition)
+      // console.log((this.data.picPosition + 5) % 10)
+      // console.log(this.data.picLenth - 5)
+      console.log(e.detail.current)
+      console.log((this.data.picPosition + 5) % this.data.pageSize == 0)
+      if ( (this.data.picPosition + 1) % this.data.pageSize == 0 && this.data.picLenth - 1 == this.data.picPosition){
+          Toast.loading({
+            duration: 0,       // 持续展示 toast
+            mask: true,
+            message: "加载中..."
+          });
+          const timer = setInterval(() => {
+            var currentPage = that.data.currentPage; // 获取当前页码
+            currentPage += 1; // 加载当前页面的下一页数据
+            that.data.currentPage = currentPage;
+            wx.request({
+              url: url_microService + '/app/search',
+              data: {
+                pageNo: currentPage,
+                longitude: '118.8',
+                latitude: '33.7',
+                distance: '1000',
+                type: '1'
+              },
+              header: {},
+              method: 'GET',
+              dataType: 'json',
+              responseType: 'text',
+              success: function (res) {
+                console.log(res)
+                console.log("分页了")
+                var row = res.data.info.rows;
+                console.log(that.arrayItems)
+                that.setData({
+                  ["arrayItems[" + (currentPage - 1) + "]"]: res.data.info.rows,
+                  picLenth: that.data.picLenth + that.data.pageSize
+                })
+                Toast.clear()
+              },
+              fail: function (res) { },
+              complete: function (res) { },
+            })
+            clearInterval(timer);
+          }, 600);
+
+      }
+    }
+  },
+//跳转筛选页面
+  onClickRight: function(){
+    wx.navigateTo({
+      url: '/pages/search/search',
+    })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
