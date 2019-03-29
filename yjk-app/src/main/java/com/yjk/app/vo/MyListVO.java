14652ab19f8d10556138f4c1c409791774f13376
huisonglin <huisonglin@LAPTOP-S81Q6GM6>
2019-03-29 17:52:43 +0800

    xx

--- yjk-app/src/main/java/com/yjk/app/vo/MyListVO.java
+++ yjk-app/src/main/java/com/yjk/app/vo/MyListVO.java
@@ -28,9 +28,29 @@ public class MyListVO {
 	
 	private String xcxOpenId;
 	
+	private Double longitude; //经度
+	
+	private Double latitude; //纬度
+	
 
 	
 	
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
 	public String getXcxOpenId() {
 		return xcxOpenId;
 	}
